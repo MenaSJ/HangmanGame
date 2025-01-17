311fa92e8e1db883572fd921275b2efa40689f4b
MenaSJ <Mena_JS9@outlook.com>
2025-01-16 22:09:48 -0600

    Primer Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+  import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div style={{
+        
+      }}>
+        <div>Lose Win</div>
+      </div>
+    </>
+  )
+}
+
+export default App
