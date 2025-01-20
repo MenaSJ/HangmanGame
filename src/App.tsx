5454fae1b972a2d2395ff6b49c9d53a8785c0092
MenaSJ <Mena_JS9@outlook.com>
2025-01-19 22:39:56 -0600

    Added first drawings

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,68 @@ import './App.css'
 function App() {
   return (
     <>
-      <div style={{
-        
-      }}>
-        <div>Lose Win</div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
+    >
+      <div>
+        Win Lose
       </div>
+      <div    // hangman
+        style={{
+          position: "relative",
+          margin: "0 auto"
+        }}
+      >
+        <div
+          style={{    // head
+            width: "50px",
+            height: "50px",
+            position: "absolute",
+            top: "50px",
+            right: "-30px",
+            border: "10px solid black",
+            borderRadius: "100%"
+          }}
+        />
+        <div    // right vertical bar
+          style={{
+            background: "black",
+            width: "10px",
+            height: "50px",
+            position: "absolute",
+            right: "0",
+            top: "0"
+          }}
+        />
+        <div    // top horizontal bar
+          style={{
+            background: "black",
+            width: "200px",
+            height: "10px"
+          }}
+        />
+        <div    // left vertical bar
+          style={{
+            background: "black",
+            width: "10px",
+            height: "400px"
+          }}
+        />
+        <div    // bottom hotizontal bar
+          style={{
+            background: "black",
+            width: "250px",
+            height: "10px",
+            position: "absolute",
+            left: "-125px",
+          }}
+        />
+      </div>
+    </div>
     </>
   )
 }
